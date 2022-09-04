e54070014a37b8ce22b98dd483fa1db396c85bd2
strozzi <stroz.dennis@gmail.com>
2022-09-04 15:48:40 +0200

    lernen diesdas

--- src/main/java/katalog_lernen/Katalog.java
+++ src/main/java/katalog_lernen/Katalog.java
@@ -1,4 +1,36 @@
 package katalog_lernen;
 
 public class Katalog {
+
+    private final int SCHRITTWEITE = 3;
+    private final int INITIALGROESSE = 2;
+
+    private int index;
+
+    private Buch[] buecher;
+
+    public Katalog(){
+        buecher = new Buch[INITIALGROESSE];
+        index = 0;
+    }
+
+    public void fuegeHinzu(Buch b){
+        if(index >= buecher.length){
+            this.vergroeßere();
+        }
+        buecher[index] = b;
+    }
+
+    public void vergroeßere() {
+        Buch[] neu = new Buch[buecher.length + SCHRITTWEITE];
+        buecher = neu;
+    }
+
+    public void gibBuecher throws NullPointerException(){
+        try(NullPointerException n){
+
+        }
+
+    }
+
 }
